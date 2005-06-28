776941d4f7ac0b9b7337623864066fa2436c8a35
Alistair Young <alistair@codebrane.com>
2005-06-28 11:33:45 +0000

    Guanxi definitions

---
+++ src/org/guanxi/common/definitions/Guanxi.java
@@ -0,0 +1,18 @@
+/* CVS Header
+   $Id$
+   $Log$
+   Revision 1.1  2005/06/28 11:33:45  alistairskye
+   Guanxi definitions
+
+*/
+
+package org.Guanxi.Common.Definitions;
+
+/**
+ * <font size=5><b></b></font>
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
+public class Guanxi {
+  public static final String NS_SP_NAME_IDENTIFIER = "urn:guanxi:sp";
+}
