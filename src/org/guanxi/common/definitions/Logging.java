bf5516dd7526c7d145fc835ee9c44a9ada5ebaa4
Alistair Young <alistair@codebrane.com>
2005-07-21 09:14:02 +0000

    System wide Logging definitions

---
+++ src/org/guanxi/common/definitions/Logging.java
@@ -0,0 +1,20 @@
+/* CVS Header
+   $Id$
+   $Log$
+   Revision 1.1  2005/07/21 09:14:02  alistairskye
+   System wide Logging definitions
+
+*/
+
+package org.guanxi.common.definitions;
+
+/**
+ * <font size=5><b></b></font>
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
+public class Logging {
+  public static final String DEFAULT_LOG_DIR = "/WEB-INF/logs/";
+  public static final String DEFAULT_CONFIG_FILE = "/WEB-INF/config/log4j.xml";
+  public static final String DEFAULT_LAYOUT = "%d{dd MMMM yyyy HH:mm:ss} - %m%n";
+}
