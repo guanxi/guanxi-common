6a08016f5d7efb53f231a00ec1f6e8ba369626e8
Alistair Young <alistair@codebrane.com>
2005-08-11 14:18:52 +0000

    SAML attribute parser and container

---
+++ src/org/guanxi/common/Bag.java
@@ -0,0 +1,128 @@
+/* CVS Header
+   $Id$
+   $Log$
+   Revision 1.1  2005/08/11 14:18:52  alistairskye
+   SAML attribute parser and container
+
+*/
+
+package org.guanxi.common;
+
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+import org.guanxi.samuel.utils.ParseErrorHandler;
+import org.guanxi.samuel.utils.Resolver;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.SAXParser;
+import java.util.HashMap;
+
+/**
+ * <font size=5><b></b></font>
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
+public class Bag {
+  /** The Guard session that this bag of attributes is for */
+  private String sessionID = null;
+  private String assertionID = null;
+  private String issueInstant = null;
+  private String issuer = null;
+  private String majorVersion = null;
+  private String minorVersion = null;
+  private StringBuffer value = new StringBuffer();
+  HashMap attributes = null;
+
+  public Bag(InputSource in) throws GuanxiException {
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    factory.setNamespaceAware(true);
+    factory.setValidating(true);
+
+    try {
+      String[] schemas = {"soap-envelope.xsd",
+                          "oasis-sstc-saml-schema-assertion-1.1.xsd",
+                          "oasis-sstc-saml-schema-protocol-1.1.xsd",
+                          "saml-schema-protocol-2.0.xsd",
+                          "saml-schema-assertion-2.0.xsd"};
+      SAXParser parser = factory.newSAXParser();
+      XMLReader xmlReader = parser.getXMLReader();
+      xmlReader.setFeature("http://apache.org/xml/features/validation/schema", true);
+      xmlReader.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", schemas);
+      xmlReader.setEntityResolver(new Resolver());
+      xmlReader.setErrorHandler(new ParseErrorHandler());
+      xmlReader.setContentHandler(new BagContentHandler());
+      xmlReader.parse(in);
+    }
+    catch(Exception e) {
+      throw new GuanxiException(e);
+    }
+  }
+
+  public String getSessionID() { return sessionID; }
+  public String getAssertionID() { return assertionID; }
+  public String getIssueInstant() { return issueInstant; }
+  public String getIssuer() { return issuer; }
+  public String getMajorVersion() { return majorVersion; }
+  public String getMinorVersion() { return minorVersion; }
+
+  class BagContentHandler extends DefaultHandler {
+    boolean inAssertion, inGuanxiGuardSessionID;
+
+    public void startElement(String uri, String localName, String qName, Attributes attrs) {
+      if (localName.equals("GuanxiGuardSessionID")) {
+        inGuanxiGuardSessionID = true;
+      }
+
+      if (localName.equals("Assertion")) {
+        assertionID = getAttribute(attrs, "AssertionID");
+        issueInstant = getAttribute(attrs, "IssueInstant");
+        issuer = getAttribute(attrs, "Issuer");
+        majorVersion = getAttribute(attrs, "MajorVersion");
+        minorVersion = getAttribute(attrs, "MinorVersion");
+
+        inAssertion = true;
+      }
+    }
+
+    public void endElement(String uri, String localName, String qName) {
+      if (inGuanxiGuardSessionID) {
+        sessionID = getValue();
+        inGuanxiGuardSessionID = false;
+      }
+
+      value = new StringBuffer();
+    }
+
+    public void characters(char[] chars, int start, int length) {
+      /**
+       * We're currently processing an element. All character data from now until
+       * the next endElement() call will be the data for this  element.
+       */
+      value.append(chars, start, length);
+    }
+
+    public String getValue() {
+      return value.toString().trim();
+    }
+
+    String getAttribute(Attributes attrs, String name) {
+      for (int count=0; count < attrs.getLength(); count++) {
+        if (attrs.getLocalName(count).equals(name)) {
+          return attrs.getValue(count);
+        }
+      }
+
+      return "";
+    }
+  }
+
+  public void addAttribute(String name, String value) {
+    if (attributes.containsKey(name)) {
+      String buffer = (String)attributes.get(name);
+      buffer += ";" + value;
+      attributes.remove(name);
+      attributes.put(name, buffer);
+    }
+  }
+}
