6819bc86182b65cde0c0232d7c84b53ef32de6ee
Alistair Young <alistair@codebrane.com>
2005-08-12 12:48:31 +0000

    Added license

--- src/org/guanxi/common/Bag.java
+++ src/org/guanxi/common/Bag.java
@@ -1,6 +1,25 @@
+//: "The contents of this file are subject to the Mozilla Public License
+//: Version 1.1 (the "License"); you may not use this file except in
+//: compliance with the License. You may obtain a copy of the License at
+//: http://www.mozilla.org/MPL/
+//:
+//: Software distributed under the License is distributed on an "AS IS"
+//: basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//: License for the specific language governing rights and limitations
+//: under the License.
+//:
+//: The Original Code is Guanxi (http://www.guanxi.uhi.ac.uk).
+//:
+//: The Initial Developer of the Original Code is Alistair Young alistair@smo.uhi.ac.uk.
+//: Portions created by SMO WWW Development Group are Copyright (C) 2005 SMO WWW Development Group.
+//: All Rights Reserved.
+//:
 /* CVS Header
    $Id$
    $Log$
+   Revision 1.3  2005/08/12 12:48:31  alistairskye
+   Added license
+
    Revision 1.2  2005/08/12 09:03:14  alistairskye
    Added SAML attribute gathering
 
