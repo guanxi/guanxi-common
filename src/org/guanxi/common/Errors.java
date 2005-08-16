2f1d2c0ee6887ef98d0b98ddfd558cae7aa9029a
Alistair Young <alistair@codebrane.com>
2005-08-16 14:56:57 +0000

    Added isError()

--- src/org/guanxi/common/Errors.java
+++ src/org/guanxi/common/Errors.java
@@ -1,6 +1,9 @@
 /* CVS Header
    $Id$
    $Log$
+   Revision 1.2  2005/08/16 14:56:57  alistairskye
+   Added isError()
+
    Revision 1.1  2005/08/16 12:36:53  alistairskye
    Errors class
 
@@ -17,4 +20,15 @@ public class Errors {
   public static final String ENGINE_WAYF_LOCATION_EXCEPTION = "ENGINE_WAYF_LOCATION_EXCEPTION";
   public static final String ENGINE_WAYF_LOCATION_NO_GUARD_ID = "ENGINE_WAYF_LOCATION_NO_GUARD_ID";
   public static final String ENGINE_WAYF_LOCATION_GUARD_FAILED_VERIFICATION = "ENGINE_WAYF_LOCATION_GUARD_FAILED_VERIFICATION";
+  public static final String ENGINE_CURRENTLY_INITIALISING = "ENGINE_CURRENTLY_INITIALISING";
+
+  public static boolean isError(String text) {
+    if ((text.equals(ENGINE_WAYF_LOCATION_EXCEPTION)) ||
+        (text.equals(ENGINE_WAYF_LOCATION_NO_GUARD_ID)) ||
+        (text.equals(ENGINE_WAYF_LOCATION_GUARD_FAILED_VERIFICATION)) ||
+        (text.equals(ENGINE_CURRENTLY_INITIALISING)))
+      return true;
+    else
+      return false;
+  }
 }
