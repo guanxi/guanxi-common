22d71ffab29ffaa5276b26d58c8e98324368f97a
Alistair Young <alistair@codebrane.com>
2005-08-16 12:36:53 +0000

    Errors class

---
+++ src/org/guanxi/common/Errors.java
@@ -0,0 +1,20 @@
+/* CVS Header
+   $Id$
+   $Log$
+   Revision 1.1  2005/08/16 12:36:53  alistairskye
+   Errors class
+
+*/
+
+package org.guanxi.common;
+
+/**
+ * <font size=5><b></b></font>
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
+public class Errors {
+  public static final String ENGINE_WAYF_LOCATION_EXCEPTION = "ENGINE_WAYF_LOCATION_EXCEPTION";
+  public static final String ENGINE_WAYF_LOCATION_NO_GUARD_ID = "ENGINE_WAYF_LOCATION_NO_GUARD_ID";
+  public static final String ENGINE_WAYF_LOCATION_GUARD_FAILED_VERIFICATION = "ENGINE_WAYF_LOCATION_GUARD_FAILED_VERIFICATION";
+}
