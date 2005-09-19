1efdcfee478df45d2ba3fdc4fc1e503eaef7fb3d
Alistair Young <alistair@codebrane.com>
2005-09-19 12:09:28 +0000

    Class for mapping attributes from one form/value to another

---
+++ src/org/guanxi/common/security/AttributeMap.java
@@ -0,0 +1,121 @@
+//: "The contents of this file are subject to the Mozilla Public License
+//: Version 1.1 (the "License"); you may not use this file except in
+//: compliance with the License. You may obtain a copy of the License at
+//: http://www.mozilla.org/MPL/
+//:
+//: Software distributed under the License is distributed on an "AS IS"
+//: basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//: License for the specific language governing rights and limitations
+//: under the License.
+//:
+//: The Original Code is Guanxi (http://www.guanxi.uhi.ac.uk).
+//:
+//: The Initial Developer of the Original Code is Alistair Young alistair@smo.uhi.ac.uk.
+//: Portions created by SMO WWW Development Group are Copyright (C) 2005 SMO WWW Development Group.
+//: All Rights Reserved.
+//:
+/* CVS Header
+   $Id$
+   $Log$
+   Revision 1.1  2005/09/19 12:09:28  alistairskye
+   Class for mapping attributes from one form/value to another
+
+*/
+
+package org.guanxi.common.security;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.NamedNodeMap;
+import org.guanxi.samuel.utils.ParserPool;
+import org.guanxi.samuel.utils.XUtils;
+import org.guanxi.samuel.exception.ParserPoolException;
+import org.guanxi.common.GuanxiException;
+import org.guanxi.common.definitions.Guanxi;
+import java.io.File;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * <font size=5><b></b></font>
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
+public class AttributeMap {
+  private static final String MAP_ATTR_ID = "id";
+  private static final String MAP_ATTR_NAME = "attrName";
+  private static final String MAP_ATTR_VALUE = "attrValue";
+  private static final String MAP_ATTR_MAPPED_NAME = "mappedName";
+  private static final String MAP_ATTR_MAPPED_VALUE = "mappedValue";
+
+  ParserPool parser = null;
+  XUtils xUtils = null;
+  Document map = null;
+  NodeList mapNodes = null;
+  String mappedName, mappedValue;
+
+  public AttributeMap(String mapXMLFile) throws GuanxiException {
+    // Get a parser
+    try {
+      parser = ParserPool.getInstance();
+    }
+    catch(ParserPoolException ppe) {
+      throw new GuanxiException(ppe);
+    }
+
+    xUtils = XUtils.getInstance();
+
+    // Load up the config file
+    try {
+      map = parser.parse(new File(mapXMLFile));
+    }
+    catch(ParserPoolException ppe) {
+      throw new GuanxiException(ppe);
+    }
+
+    // Get an iterator for the <server> nodes in the config file
+    mapNodes = map.getElementsByTagNameNS(Guanxi.NS_IDP_NAME_IDENTIFIER, "map");
+  }
+
+  public boolean map(String id, String attrName, String attrValue) {
+    Node mapNode = null;
+    NamedNodeMap mapAttrs = null;
+    Pattern pattern = null;
+    Matcher matcher = null;
+
+    for (int count=0; count < mapNodes.getLength(); count++) {
+      mapNode = mapNodes.item(count);
+      mapAttrs = mapNode.getAttributes();
+
+      // Should we restrict the map to a certain mapping id?
+      if ((id == null) || (id.equals(mapAttrs.getNamedItem(MAP_ATTR_ID).getNodeValue()))) {
+        // Have we got the correct attribute to map?
+        if (attrName.equals(mapAttrs.getNamedItem(MAP_ATTR_NAME).getNodeValue())) {
+          pattern = Pattern.compile(mapAttrs.getNamedItem(MAP_ATTR_VALUE).getNodeValue());
+          matcher = pattern.matcher(attrValue);
+
+          // Match the value of the attribute before mapping
+          if (matcher.find()) {
+            // Rename the attribute...
+            mappedName = mapAttrs.getNamedItem(MAP_ATTR_MAPPED_NAME).getNodeValue();
+            // ...and transform the value
+            mappedValue = mapAttrs.getNamedItem(MAP_ATTR_MAPPED_VALUE).getNodeValue();
+
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  public String getMappedName() {
+    return mappedName;
+  }
+
+  public String getMappedValue() {
+    return mappedValue;
+  }
+}
