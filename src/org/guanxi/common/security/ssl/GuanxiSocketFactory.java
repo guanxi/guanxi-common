b6294195f47c1d9d857205d5f0e5303cac8761ad
Alistair Young <alistair@codebrane.com>
2006-07-26 09:38:36 +0000

    Added license

--- src/org/guanxi/common/security/ssl/GuanxiSocketFactory.java
+++ src/org/guanxi/common/security/ssl/GuanxiSocketFactory.java
@@ -1,6 +1,25 @@
+//: "The contents of this file are subject to the Mozilla Public License
+//: Version 1.1 (the "License"); you may not use this file except in
+//: compliance with the License. You may obtain a copy of the License at
+//: http://www.mozilla.org/MPL/
+//:
+//: Software distributed under the License is distributed on an "AS IS"
+//: basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//: License for the specific language governing rights and limitations
+//: under the License.
+//:
+//: The Original Code is Guanxi (http://www.guanxi.uhi.ac.uk).
+//:
+//: The Initial Developer of the Original Code is Alistair Young alistair@smo.uhi.ac.uk.
+//: Portions created by SMO WWW Development Group are Copyright (C) 2005 SMO WWW Development Group.
+//: All Rights Reserved.
+//:
 /* CVS Header
    $Id$
    $Log$
+   Revision 1.4  2006/07/26 09:38:10  alistairskye
+   Added license
+
    Revision 1.3  2006/07/26 09:29:50  alistairskye
    Updated javadocs
    Updated initSSLContext() to pass new probing parameter to the SSL layer
