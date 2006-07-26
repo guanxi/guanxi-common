b36ce41b60757cb922ba5b13490b73350bd90c6f
Alistair Young <alistair@codebrane.com>
2006-07-26 09:27:24 +0000

    Updated javadocs

--- src/org/guanxi/common/security/ssl/SSL.java
+++ src/org/guanxi/common/security/ssl/SSL.java
@@ -17,6 +17,9 @@
 /* CVS Header
    $Id$
    $Log$
+   Revision 1.4  2006/07/26 09:27:24  alistairskye
+   Updated javadocs
+
    Revision 1.3  2006/07/26 09:17:51  alistairskye
    Added new boolean parameter to getTrustManagers(). If this is true then the method will return a special Guanxi TrustManager to allow for probing servers for their certificates
 
@@ -34,12 +37,31 @@ import javax.net.ssl.*;
 import java.security.KeyStore;
 import java.io.FileInputStream;
 
+/**
+ * Defines trust and keystore managers for HTTPS connections.
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
 public class SSL {
+  /**
+   * Returns an array of TrustManager objects that the JVM will use to verify or authenticate a remote
+   * server when using HTTPS.
+   *
+   * @param trustStore The full path and name of the truststore to use for server verification.
+   * Can be null if probeForServerCert is true.
+   * @param password The password for the truststore.
+   * Can be null if probeForServerCert is true.
+   * @param probeForServerCert If this is true, then an instance of GuanxiX509TrustManager will
+   * be returned. This is to allow probing servers to extract their certificates.
+   * @return An array of TrustManage objects that can be used by the JVM to verify an HTTPS connection
+   *  or null if an error occurred.
+   */
   public static TrustManager[] getTrustManagers(String trustStore, String password, boolean probeForServerCert) {
     try {
+      // If we're just after the server's certificate, delegate trust checking to Guanxi
       if (probeForServerCert)
         return new TrustManager[]{new GuanxiX509TrustManager()};
-      
+
       // First, get the default TrustManagerFactory.
       String alg = TrustManagerFactory.getDefaultAlgorithm();
       TrustManagerFactory tmFact = TrustManagerFactory.getInstance(alg);
@@ -62,6 +84,18 @@ public class SSL {
     }
   }
 
+  /**
+   * Returns an array of GuanxiX509KeyManager objects that can be used to added certificates to an HTTPS connection.
+   * The Engine uses this method to load a keystore belonging to a Guard, in order to add the Guard's
+   * certificate to the HTTPS request to, for example, an AttributeAuthority.
+   * Note that only X509KeyManager instances are supported.
+   *
+   * @param guardID The ID of the Guard who's keystore must be loaded. This is also the alias within the keystore
+   * under which the Guard's certificate is stored. It's this certificate that's added to the HTTPS connection.
+   * @param keystore The full path and name of the keystore to load.
+   * @param password The password for the keystore.
+   * @return An array of GuanxiX509KeyManager objects or null if an error occurred.
+   */
   public static KeyManager[] getKeyManagers(String guardID, String keystore, String password) {
     try {
       String alg = KeyManagerFactory.getDefaultAlgorithm();
