5d00ca957e1c50c5879a131e085b81a315b0543c
Alistair Young <alistair@codebrane.com>
2007-01-04 13:38:25 +0000

    Rewritten to remove SAX parsing of raw SOAP message from Engine. Now stores original SAML Response as a String.

--- src/org/guanxi/common/Bag.java
+++ src/org/guanxi/common/Bag.java
@@ -17,6 +17,10 @@
 /* CVS Header
    $Id$
    $Log$
+   Revision 1.5  2007/01/04 13:38:25  alistairskye
+   Rewritten to remove SAX parsing of raw SOAP message from Engine.
+   Now stores original SAML Response as a String.
+
    Revision 1.4  2006/01/23 12:53:50  alistairskye
    Updated to use SAMUEL parser properties
 
@@ -33,20 +37,8 @@
 
 package org.guanxi.common;
 
-import org.xml.sax.XMLReader;
-import org.xml.sax.InputSource;
-import org.xml.sax.Attributes;
-import org.xml.sax.helpers.DefaultHandler;
-import org.guanxi.samuel.utils.ParseErrorHandler;
-import org.guanxi.samuel.utils.Resolver;
-import org.guanxi.samuel.definitions.SAMUEL;
-
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.SAXParser;
-import java.util.HashMap;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.ResourceBundle;
 
 /**
  * <font size=5><b></b></font>
@@ -54,150 +46,81 @@ import java.util.ResourceBundle;
  * @author Alistair Young alistair@smo.uhi.ac.uk
  */
 public class Bag {
-  /** The Guard session that this bag of attributes is for */
-  private String sessionID = null;
-  private String assertionID = null;
-  private String issueInstant = null;
-  private String issuer = null;
-  private String majorVersion = null;
-  private String minorVersion = null;
-  private StringBuffer value = new StringBuffer();
+  /** The value used as a delimiter in multi-valued attributes */
+  public static final String ATTR_VALUE_DELIM = ";";
+
+  /** String representing the raw SAML Response */
+  private String samlResponse = null;
+  /** The attributes as Strings */
   private Hashtable attributes = null;
-  private static ResourceBundle parserProps = null;
 
-  static {
-    parserProps = ResourceBundle.getBundle(SAMUEL.PARSER_PROPS_FILE);
-  }
-
-  public Bag(InputSource in) throws GuanxiException {
-    SAXParserFactory factory = SAXParserFactory.newInstance();
-    factory.setNamespaceAware(true);
-
-    if (parserProps.getString(SAMUEL.PARSER_PROPS_SCHEMA_VALIDATION).equalsIgnoreCase("true"))
-      factory.setValidating(true);
-    else
-      factory.setValidating(false);
-
+  public Bag() {
     attributes = new Hashtable();
-
-    try {
-      String[] schemas = {"soap-envelope.xsd",
-                          "oasis-sstc-saml-schema-assertion-1.1.xsd",
-                          "oasis-sstc-saml-schema-protocol-1.1.xsd",
-                          "saml-schema-protocol-2.0.xsd",
-                          "saml-schema-assertion-2.0.xsd"};
-      SAXParser parser = factory.newSAXParser();
-      XMLReader xmlReader = parser.getXMLReader();
-
-      if (parserProps.getString(SAMUEL.PARSER_PROPS_SCHEMA_VALIDATION).equalsIgnoreCase("true")) {
-        xmlReader.setFeature("http://apache.org/xml/features/validation/schema", true);
-        xmlReader.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", schemas);
-      }
-      xmlReader.setEntityResolver(new Resolver());
-      xmlReader.setErrorHandler(new ParseErrorHandler());
-      xmlReader.setContentHandler(new BagContentHandler());
-      xmlReader.parse(in);
-    }
-    catch(Exception e) {
-      throw new GuanxiException(e);
-    }
   }
 
-  public String getSessionID() { return sessionID; }
-  public String getAssertionID() { return assertionID; }
-  public String getIssueInstant() { return issueInstant; }
-  public String getIssuer() { return issuer; }
-  public String getMajorVersion() { return majorVersion; }
-  public String getMinorVersion() { return minorVersion; }
+  /**
+   * Determines whether this Bag contains any attributes.
+   *
+   * @return true if this Bag contains attributes otherwise false
+   */
+  public boolean hasAttributes() {
+    return !attributes.isEmpty();
+  }
 
+  /**
+   * Adds an attribute to the Bag. The method takes care of multiple valued
+   * attributes. Attributes with multiple values have their individual values
+   * separated by ATTR_VALUE_DELIM.
+   * 
+   * @param name The name of the attribute to add
+   * @param value The attribute's value
+   */
+  public void addAttribute(String name, String value) {
+    if (attributes.containsKey(name)) {
+      String buffer = (String)attributes.get(name);
+      buffer += ATTR_VALUE_DELIM + value;
+      attributes.remove(name);
+      attributes.put(name, buffer);
+    }
+    else
+      attributes.put(name, value);
+  }
+
+  /**
+   * Returns the value of the specified attribute. If the attribute has
+   * multiple values, they will be separated by ATTR_VALUE_DELIM.
+   *
+   * @param name The attribute name
+   * @return The values of the attribute, separated by ATTR_VALUE_DELIM if
+   * there are multiple values.
+   */
   public String getAttributeValue(String name) {
     return (String)attributes.get(name);
   }
 
+  /**
+   * Returns all the names of the attributes stored in this Bag.
+   *
+   * @return Enumeration listing all stored attribute names.
+   */
   public Enumeration getAttributeNames() {
     return attributes.keys();
   }
 
-  class BagContentHandler extends DefaultHandler {
-    String name = null;
-    boolean inAssertion, inGuanxiGuardSessionID, inAttribute, inAttributeValue;
+  /**
+   * Sets the raw SAML Response. This can be used by applications to parse to
+   * an object model.
+   *
+   * @param samlResponse String containing the raw SAML Response from which the
+   * attributes have been extracted and passed to addAttribute.
+   */
+  public void setSamlResponse(String samlResponse) { this.samlResponse = samlResponse; }
 
-    public void startElement(String uri, String localName, String qName, Attributes attrs) {
-      if (localName.equals("GuanxiGuardSessionID")) {
-        inGuanxiGuardSessionID = true;
-      }
-
-      if (localName.equals("Assertion")) {
-        assertionID = getAttribute(attrs, "AssertionID");
-        issueInstant = getAttribute(attrs, "IssueInstant");
-        issuer = getAttribute(attrs, "Issuer");
-        majorVersion = getAttribute(attrs, "MajorVersion");
-        minorVersion = getAttribute(attrs, "MinorVersion");
-
-        inAssertion = true;
-      }
-
-      if (localName.equals("Attribute")) {
-        name = getAttribute(attrs, "AttributeName");
-
-        inAttribute = true;
-      }
-
-      if (localName.equals("AttributeValue")) {
-        inAttributeValue = true;
-      }
-    }
-
-    public void endElement(String uri, String localName, String qName) {
-      if (inGuanxiGuardSessionID) {
-        sessionID = getValue();
-        inGuanxiGuardSessionID = false;
-      }
-
-      if (localName.equals("Attribute")) {
-        inAttribute = false;
-      }
-
-      if (localName.equals("AttributeValue")) {
-        addSAMLAttribute(name, getValue());
-
-        inAttributeValue = false;
-      }
-
-      value = new StringBuffer();
-    }
-
-    public void characters(char[] chars, int start, int length) {
-      /**
-       * We're currently processing an element. All character data from now until
-       * the next endElement() call will be the data for this  element.
-       */
-      value.append(chars, start, length);
-    }
-
-    public String getValue() {
-      return value.toString().trim();
-    }
-
-    String getAttribute(Attributes attrs, String name) {
-      for (int count=0; count < attrs.getLength(); count++) {
-        if (attrs.getLocalName(count).equals(name)) {
-          return attrs.getValue(count);
-        }
-      }
-
-      return "";
-    }
-
-    void addSAMLAttribute(String name, String value) {
-      if (attributes.containsKey(name)) {
-        String buffer = (String)attributes.get(name);
-        buffer += ";" + value;
-        attributes.remove(name);
-        attributes.put(name, buffer);
-      }
-      else
-        attributes.put(name, value);
-    }
-  }
+  /**
+   * Retrieves the raw SAML Response from which the attributes stored in this bag were
+   * extracted.
+   *
+   * @return String representing the raw SAML Response. 
+   */
+  public String getSamlResponse() { return samlResponse; }
 }
