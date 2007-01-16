905245bcc299060d0b4fa9a916fcfe79483ede50
Alistair Young <alistair@codebrane.com>
2007-01-16 09:30:48 +0000

    SAML definitions

---
+++ src/org/guanxi/common/definitions/SAML.java
@@ -0,0 +1,18 @@
+/* CVS Header
+   $Id$
+   $Log$
+   Revision 1.1  2007/01/16 09:30:48  alistairskye
+   SAML definitions
+
+*/
+
+package org.guanxi.common.definitions;
+
+public class SAML {
+  // URNs
+  public static final String URN_AUTH_METHOD_PASSWORD = "urn:oasis:names:tc:SAML:1.0:am:password";
+  public static final String URN_CONFIRMATION_METHOD_BEARER = "urn:oasis:names:tc:SAML:1.0:cm:bearer";
+
+  // Namespaces
+  public static final String NS_SAML_ASSERTION = "urn:oasis:names:tc:SAML:1.0:assertion";
+}
