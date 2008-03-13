338b1a8f3fbd4384c0b533339d5e52d7653bc1c8
Alistair Young <alistair@codebrane.com>
2008-03-13 17:30:28 +0000

    Updated to 1.3.6

--- deploy-jar.sh
+++ deploy-jar.sh
@@ -1,8 +1,8 @@
 mvn deploy:deploy-file \
-    -Dfile=target/guanxi-common-1.3.5.jar \
+    -Dfile=target/guanxi-common-1.3.6.jar \
     -DgroupId=org.guanxi \
     -DartifactId=guanxi-common \
-    -Dversion=1.3.5 \
+    -Dversion=1.3.6 \
     -Dpackaging=jar \
     -DgeneratePom=true \
     -DrepositoryId=source.uhi.ac.uk-repository \
