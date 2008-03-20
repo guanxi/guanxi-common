f9a186a1701d7ed2d13afa63bccb77481c55fdee
Alistair Young <alistair@codebrane.com>
2008-03-20 22:56:08 +0000

    Deleted deploy-Jar.sh. Now use mvn deploy

--- deploy-jar.sh
+++
@@ -1,9 +0,0 @@
-mvn deploy:deploy-file \
-    -Dfile=target/guanxi-common-1.3.6.jar \
-    -DgroupId=org.guanxi \
-    -DartifactId=guanxi-common \
-    -Dversion=1.3.6 \
-    -Dpackaging=jar \
-    -DgeneratePom=true \
-    -DrepositoryId=source.uhi.ac.uk-repository \
-    -Durl=dav:http://source.uhi.ac.uk/mvn
