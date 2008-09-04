89029c2ef807c15fc56c3d10f3e22c4bbf1d56aa
Alistair Young <alistair@codebrane.com>
2008-09-04 21:08:48 +0000

    Removed logging

--- src/org/guanxi/common/job/GuanxiJobConfig.java
+++ src/org/guanxi/common/job/GuanxiJobConfig.java
@@ -1,13 +1,20 @@
-/* CVS Header
-   $
-   $
-*/
+//: "The contents of this file are subject to the Mozilla Public License
+//: Version 1.1 (the "License"); you may not use this file except in
+//: compliance with the License. You may obtain a copy of the License at
+//: http://www.mozilla.org/MPL/
+//:
+//: Software distributed under the License is distributed on an "AS IS"
+//: basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//: License for the specific language governing rights and limitations
+//: under the License.
+//:
+//: The Original Code is Guanxi (http://www.guanxi.uhi.ac.uk).
+//:
+//: The Initial Developer of the Original Code is Alistair Young alistair@codebrane.com
+//: All Rights Reserved.
+//:
 
 package org.guanxi.common.job;
-
-import org.apache.log4j.Logger;
-import org.guanxi.common.log.Log4JLoggerConfig;
-import org.guanxi.common.log.Log4JLogger;
 
 /**
  * Represents core configuration information passed to jobs. All jobs get this information.
@@ -23,15 +30,6 @@ public interface GuanxiJobConfig {
   public String getCronLine();
   public void setJobClass(String jobClass);
   public String getJobClass();
-
-  public void setLog(Logger log);
-  public Logger getLog();
-
-  public void setLoggerConfig(Log4JLoggerConfig loggerConfig);
-  public Log4JLoggerConfig getLoggerConfig();
-
-  public void setLogger(Log4JLogger logger);
-  public Log4JLogger getLogger();
 
   public void setPrivateData(Object privateData);
   public Object getPrivateData();
