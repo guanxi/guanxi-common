c80111f051d64216c9505308874d9bc7c724c8d3
Alistair Young <alistair@codebrane.com>
2009-02-27 15:18:50 +0000

    Updated to test escaping, from matthew

--- test/org/guanxi/test/common/filters/GenericEncoderTest.java
+++ test/org/guanxi/test/common/filters/GenericEncoderTest.java
@@ -96,4 +96,112 @@ public class GenericEncoderTest {
 			output.delete(0, output.length());
 		}
 	}
+	
+	/**
+	 * This test that it is possible to replace every character
+	 * with the same escaped character.
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testUniformEscaping() throws IOException {
+		StringWriter writer;
+		StringBuffer output;
+		GenericEncoderSubclass encoder;
+		StringBuffer comparisonGeneration;
+		String comparison;
+		
+		writer = new StringWriter();
+		output = writer.getBuffer();
+		encoder = new GenericEncoderSubclass(writer) {
+
+			@Override
+			protected String escape(char c) {
+				return "a";
+			}
+
+			@Override
+			protected boolean requiresEscaping(char c) {
+				return true;
+			}
+			
+		};
+		
+		comparisonGeneration = new StringBuffer();
+		for ( int i = 0;i < 100;i++ ) {
+			comparisonGeneration.append("a");
+		}
+		comparison = comparisonGeneration.toString();
+		
+		for ( int i = 0;i < 100;i++ ) {
+			encoder.write(TestUtils.randomString(100));
+			assertEquals("GenericEncoder has altered the data inappropriately", comparison, output.toString());
+			output.delete(0, output.length());
+		}
+	}
+	
+	/**
+	 * This tests the ability of the GenericEncoder to escape specific
+	 * characters and not others.
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testSelectiveEscaping() throws IOException {
+		StringWriter writer;
+		StringBuffer output;
+		GenericEncoderSubclass encoder;
+		String input;
+		final char[][] encoding;
+		
+		// this stores the mapping between unescaped and escaped characters
+		// this is stored in a format which makes it easy for the input string
+		// to be manipulated outside of the encoder. This leads to a suboptimal
+		// encoder implementation.
+		encoding = new char[][]{
+				new char[]{ '1', 'a' },
+				new char[]{ '2', 'b' },
+				new char[]{ '3', 'c' },
+				new char[]{ '4', 'd' },
+				new char[]{ '5', 'e' },
+				new char[]{ '6', 'f' },
+				new char[]{ '7', 'g' },
+				new char[]{ '8', 'h' },
+				new char[]{ '9', 'i' },
+				new char[]{ '0', 'j' },
+		};
+		
+		writer = new StringWriter();
+		output = writer.getBuffer();
+		encoder = new GenericEncoderSubclass(writer) {
+
+			@Override
+			protected String escape(char c) {
+				for ( int i = 0;i < encoding.length;i++ ) {
+					if ( encoding[i][0] == c ) {
+						return encoding[i][1] + "";
+					}
+				}
+				return c + "";
+			}
+
+			@Override
+			protected boolean requiresEscaping(char c) {
+				return Character.isDigit(c);
+			}
+			
+		};
+		
+		for ( int i = 0;i < 100;i++ ) {
+			input = TestUtils.randomString(100);
+			
+			encoder.write(input);
+			for ( int j = 0;j < encoding.length;j++ ) {
+				input.replace(encoding[i][0], encoding[i][1]);
+			}
+			
+			assertEquals("GenericEncoder has altered the data inappropriately", input, output.toString());
+			output.delete(0, output.length());
+		}
+	}
 }
