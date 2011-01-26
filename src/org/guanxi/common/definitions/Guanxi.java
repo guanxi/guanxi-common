26dc525eb3e30a05befa50db8cd9731496535454
Alistair Young <alistair@codebrane.com>
2011-01-26 09:24:29 +0000

    Added Discovery Feed context definition

--- src/org/guanxi/common/definitions/Guanxi.java
+++ src/org/guanxi/common/definitions/Guanxi.java
@@ -120,6 +120,8 @@ public class Guanxi {
   public static final String CONTEXT_ATTR_IDP_ENTITY_FARM = "CONTEXT_ATTR_IDP_ENTITY_FARM";
   /** The servlet context attribute that holds the EntityFarm implementation for the Engine */
   public static final String CONTEXT_ATTR_ENGINE_ENTITY_FARM = "CONTEXT_ATTR_ENGINE_ENTITY_FARM";
+  /** The servlet context attribute that holds the DiscoveryFeedManager for the Engine */
+  public static final String CONTEXT_ATTR_ENGINE_DISCOVERY_FEED_MANAGER = "CONTEXT_ATTR_ENGINE_DISCOVERY_FEED_MANAGER";
 
   /** The name of the request parameter that the Engine and Guard use to add and retrieve the
    *  SAML attributes when the Engine POSTs them to the Guard.
